1b2081ece9806b7d7f40b866386584ef402ab1eb
Joe Moloney <joe.moloney496@gmail.com>
2026-07-21 10:14:23 +0100

    borrowing

--- arraysandslices.rs
+++ arraysandslices.rs
@@ -17,7 +17,7 @@ fn main() {
     let xs: [i32; 5] = [1, 2, 3, 4, 5];
 
     // All elements can be initialised to the same value
-    let ys: [i32, 500] = [0; 500];
+    let ys: [i32; 500] = [0; 500];
 
     println!("First element of the XS array: {}", xs[0]);
     println!("Second element of the XS array: {}", xs[1]);
@@ -26,7 +26,23 @@ fn main() {
     println!("Number of elements in array: {}", xs.len());
 
     // Arrays are stack allocated (as opposed to the heap)
+    println!("XS Array occupies {} bytes", mem::size_of_val(&xs));
     // Note: The stack is very fast and is where memory is allocated by default in Rust. The allocation is local to a function call however and is limited in size
     // The heap on the other hand is slower and is explicitally allocated by your program but is effectively unlimited in size and is globally accessible
-    println!("Array occupies {} bytes", mem::size_of_val(&xs));
+
+    //Arrays can be borrowed as slices
+    println!("Borrow the whole array as a slice.");
+    analyse_slice(&xs);
+
+    // Slices can point to a section of an array
+    // They are of the form [starting_index..ending_index]
+    // 'starting_index' is the first position in the slice
+    // 'ending_index' is one more than the last position in the slice
+    println!("Borrow a section of the array as a slice");
+    analyse_slice(&ys[1 .. 4]);
+
+    // Example of empty slice '&[]':
+    let empty_array: [u32; 0] = [];
+    assert_eq!(&empty_array, &[]);
+    assert_eq!(&empty_array, &[][..]); // Same but more verbose
 }
